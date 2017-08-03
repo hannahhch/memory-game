7b2c3adbc567fbbc6a02d37e6529c1b7db168e95
Hannah Hudson <hannah.hch@gmail.com>
2017-08-03 15:51:29 -0400

    thing flip badly

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-var cardImages = [];
-cardImages[0] = 'url(images/art.jpg)',
-cardImages[1] = 'url(images/art.jpg)',
-cardImages[2] = 'url(images/bridge.jpg)',
-cardImages[3] = 'url(images/bridge.jpg',
-cardImages[4] = 'url(images/bullbike.jpg',
-cardImages[5] = 'url(images/bullbike.jpg',
-cardImages[6] = 'url(images/durhambulls.jpg',
-cardImages[7] = 'url(images/durhambulls.jpg',
-cardImages[8] = 'url(images/smokestack.jpg',
-cardImages[9] = 'url(images/smokestack.jpg',
-cardImages[10] = 'url(images/watertower.jpg',
-cardImages[11] = 'url(images/watertower.jpg';
+let cardImages = [
+'url(images/art.jpg)',
+'url(images/art.jpg)',
+'url(images/bridge.jpg)',
+'url(images/bridge.jpg)',
+'url(images/bullbike.jpg)',
+'url(images/bullbike.jpg)',
+'url(images/durhambulls.jpg)',
+'url(images/durhambulls.jpg)',
+'url(images/smokestack.jpg)',
+'url(images/smokestack.jpg)',
+'url(images/watertower.jpg)',
+'url(images/watertower.jpg)'];
 
 
-/*will only change the first image*/
+/*changes the an image when clicked*/
 
 function changeImage(){
   for (let i = 0; i < cardImages.length; i ++) {
     this.style.backgroundImage = cardImages[i];
   }
-
 }
-
+/* I had to target each single button, thats*/
 let changeOne = document.getElementById('one');
 changeOne.addEventListener('click', changeImage);
 
@@ -30,3 +29,48 @@ changeTwo.addEventListener('click', changeImage);
 
 let changeThree = document.getElementById('three');
 changeThree.addEventListener('click', changeImage);
+
+let changeFour = document.getElementById('four');
+changeFour.addEventListener('click', changeImage);
+
+let changeFive = document.getElementById('five');
+changeFive.addEventListener('click', changeImage);
+
+let changeSix = document.getElementById('six');
+changeSix.addEventListener('click', changeImage);
+
+let changeSeven = document.getElementById('seven');
+changeSeven.addEventListener('click', changeImage);
+
+let changeEight = document.getElementById('eight');
+changeEight.addEventListener('click', changeImage);
+
+let changeNine = document.getElementById('nine');
+changeNine.addEventListener('click', changeImage);
+
+let changeTen = document.getElementById('ten');
+changeTen.addEventListener('click', changeImage);
+
+let changeEleven = document.getElementById('eleven');
+changeEleven.addEventListener('click', changeImage);
+
+let changeTwelve = document.getElementById('twelve');
+changeTwelve.addEventListener('click', changeImage);
+
+let changeThirteen = document.getElementById('thirteen');
+changeThirteen.addEventListener('click', changeImage);
+
+let changeFourteen = document.getElementById('fourteen');
+changeFourteen.addEventListener('click', changeImage);
+
+let changeFifteen = document.getElementById('fifteen');
+changeFifteen.addEventListener('click', changeImage);
+
+let changeSixteen = document.getElementById('sixteen');
+changeSixteen.addEventListener('click', changeImage);
+
+let changeSeventeen = document.getElementById('seventeen');
+changeSeventeen.addEventListener('click', changeImage);
+
+let changeEighteen = document.getElementById('eighteen');
+changeEighteen.addEventListener('click', changeImage);
