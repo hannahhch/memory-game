9a5ca5e9b4e1733d44453eb6b3bb3fbff813c0aa
Hannah Hudson <hannah.hch@gmail.com>
2017-08-03 14:57:27 -0400

    somethings flip

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 var cardImages = [];
 cardImages[0] = 'url(images/art.jpg)',
 cardImages[1] = 'url(images/art.jpg)',
-cardImages[2] = ('images/bridge.jpg'),
-cardImages[3] = ('images/bridge.jpg'),
-cardImages[4] = ('images/bullbike.jpg'),
-cardImages[5] = ('images/bullbike.jpg'),
-cardImages[6] = ('images/durhambulls.jpg'),
-cardImages[7] = ('images/durhambulls.jpg'),
-cardImages[8] = ('images/smokestack.jpg'),
-cardImages[9] = ('images/smokestack.jpg'),
-cardImages[10] = ('images/watertower.jpg'),
-cardImages[11] = ('images/watertower.jpg');
+cardImages[2] = 'url(images/bridge.jpg)',
+cardImages[3] = 'url(images/bridge.jpg',
+cardImages[4] = 'url(images/bullbike.jpg',
+cardImages[5] = 'url(images/bullbike.jpg',
+cardImages[6] = 'url(images/durhambulls.jpg',
+cardImages[7] = 'url(images/durhambulls.jpg',
+cardImages[8] = 'url(images/smokestack.jpg',
+cardImages[9] = 'url(images/smokestack.jpg',
+cardImages[10] = 'url(images/watertower.jpg',
+cardImages[11] = 'url(images/watertower.jpg';
 
 
 /*will only change the first image*/
 
 function changeImage(){
-  let change = document.getElementById('one');
-  change.style.backgroundImage = cardImages[0];
+  for (let i = 0; i < cardImages.length; i ++) {
+    this.style.backgroundImage = cardImages[i];
+  }
+
 }
 
-let changeVar = document.getElementById('one');
+let changeOne = document.getElementById('one');
+changeOne.addEventListener('click', changeImage);
 
-changeVar.addEventListener('click', changeImage);
+let changeTwo = document.getElementById('two');
+changeTwo.addEventListener('click', changeImage);
+
+let changeThree = document.getElementById('three');
+changeThree.addEventListener('click', changeImage);
