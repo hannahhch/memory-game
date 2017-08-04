ac4c98ba4b480b05520c6d39980b00c8a4b2f12b
Hannah Hudson <hannah.hch@gmail.com>
2017-08-04 15:04:37 -0400

    made some arrays, they do stuff sort of

--- main.js
+++ main.js
@@ -43,6 +43,10 @@ let sml_cards = [
   'bees',
   'rocket',
   'rocket',
+  'dpac',
+  'dpac',
+  'beer',
+  'beer'
 ];
 //function set for easy mode
   //this will take the cards array and sort them into a new random array
@@ -52,6 +56,8 @@ cards = cards.sort(function (a, b) {
 
   //select all the image boxes with game_title
 let tiles = document.querySelectorAll('.game_title ');
+//make an emtpty array for later
+let newArray = [];
   //loop through array and add the class (flipped) so that they start as bricks
 for (let i = 0; i < tiles.length; i++) {
   tiles[i].classList.add('flipped');
@@ -60,8 +66,28 @@ for (let i = 0; i < tiles.length; i++) {
   //when the event target (game title) is clicked, you can toggle from bricks to image
   tiles[i].addEventListener('click', function (event) {
   event.target.classList.toggle('flipped');
+  //put the two things you clicked into a new array
+  newArray.push(event.target);
+  console.log(newArray);
+  //if the length is less than 2 (which it will be) run the function
+  if (newArray.length === 2) {
+    checkEquality();
+  }
 });
 }
+
+//you have an array that puts 2 thing into it at spot 0 and 1, if they are equal, alert.
+function checkEquality(){
+  if (newArray[0] === newArray[1]){
+    alert('I am equal');
+    //once done, remove them from the array so its clear for the next 2
+    newArray.pop();
+    newArray.pop();
+  } else {
+    newArray.pop();
+    newArray.pop();
+  }
+};
 
 //function set for hard mode (same as easy)
 sml_cards = sml_cards.sort(function (a, b) {
@@ -79,6 +105,7 @@ for (let i = 0; i < sml_tiles.length; i++) {
 });
 }
 
+
 //this is the page timer
 
 //this var will set the time to count by seconds
@@ -95,5 +122,5 @@ function countTimer() {
 //set the seconds var
    var seconds = totalSeconds - (hour * 3600 + minute * 60);
 //the the timer placeholder from html and add the hour var, minute and seconds
-   document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+   document.getElementById("timer").innerHTML = hour + " hours | " + minute + " minutes | " + seconds + " seconds";
 }
